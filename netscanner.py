1e210ab30341a69fdb4436b4c6269cc4b68db26e
Ringo <TRingo1@users.noreply.github.com>
2019-11-18 15:05:18 -0500

    Update netscanner.py

--- netscanner.py
+++ netscanner.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env
 
-import scapyall as scapy
+import scapy.all as scapy
 
 def scan(ip):
-  arp_request = scapy.ARP()  
-  print(arp_requst.summary())   
-         
+  arp_request = scapy.ARP(pdst=ip) 
+  arp_request.show()
+  broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+  broadcast.show()
+  arp_request_broadcast = broadcast/arp_request
+  arp_request_broadcast.show()
+  answeredlist, unansweredlist = scapy.srp(arp_request_braodcast)
+  # to list variables in scapy Ether 
+  scapy.ls(Ether)
+#list out the answered responses  
+  print(answeredlist.summary())
+  
 scan("192.168.0.0/16")
         
