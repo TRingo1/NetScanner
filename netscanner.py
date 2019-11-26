b3593bf8a371e970b36c803d8640eef24657d51a
Ringo <TRingo1@users.noreply.github.com>
2019-11-25 22:13:19 -0500

    updated function to argparse

--- netscanner.py
+++ netscanner.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env
 
 import scapy.all as scapy
+import argparse
+
+def get_arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-t", "--target", dest="target", help="Target IP /IP range.")
+  options = parser.parse_args()
+  return options
+
 
 def scan(ip):
   arp_request = scapy.ARP(pdst=ip) 
@@ -22,5 +30,6 @@ def print_result(results_list):
     for client in results_list:
       print(client["ip"] + "\t\t" + client["mac"])
   
-scan_result = scan("192.168.224.1/24")
+options = get_arguments()
+scan_result = scan(options.target)
 print_result(scan_result)        
