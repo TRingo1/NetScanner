dabaa59039cd2785fc9420a9240d79ecb4795d74
Ringo <TRingo1@users.noreply.github.com>
2019-11-25 21:50:05 -0500

    updated spacing

--- netscanner.py
+++ netscanner.py
@@ -11,13 +11,13 @@ def scan(ip):
   #arp_request_broadcast.show()
   answeredlist = scapy.srp(arp_request_braodcast, timeout=1, verbose=False)[0]
 
-clients_list = []
-for element in answered_list:
-  client_dict = {"ip" : element[1].psrc, "mac": element[1].hwsrc}
-  client_list.append(client_dict)
-return (client_list)
+  clients_list = []
+  for element in answered_list:
+    client_dict = {"ip" : element[1].psrc, "mac": element[1].hwsrc}
+    client_list.append(client_dict)
+  return (client_list)
 
-def print_results(results_list)
+def print_results(results_list):
     print("IP\t\t\tMAC Address\n_______________________________________________")
     for client in results_list:
       print(client["ip"] + "\t\t" + client["mac"])
